8e9973fb11c74bde1a5b4ef6e46365fe0ac9f87c
shekhawatprateek <princeshekhawat78@gmail.com>
2026-03-30 12:25:22 +0530

    cadbury above

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
 import Skills from './components/Skills'
-import Projects from './components/Projects' // Make sure the filename matches!
+import Projects from './components/Projects' 
 import Contact from './components/Contact'
 import AITerminal from './components/AITerminal'
 import PremiumEffects from './components/PremiumEffects';
@@ -16,14 +16,12 @@ function App() {
   const lenisRef = useRef()
 
   useEffect(() => {
-    // THE FIX: This perfectly synchronizes GSAP animations with Lenis Smooth Scrolling.
-    // This eliminates all visual jitter when cards are pinning or scaling.
     function update(time) {
       lenisRef.current?.lenis?.raf(time * 1000)
     }
     
     gsap.ticker.add(update)
-    gsap.ticker.lagSmoothing(0) // Prevents GSAP from "catching up" if the tab is inactive
+    gsap.ticker.lagSmoothing(0) 
 
     return () => {
       gsap.ticker.remove(update)
@@ -34,14 +32,17 @@ function App() {
     <ReactLenis root ref={lenisRef} autoRaf={false} options={{ lerp: 0.1, duration: 1.5 }}>
       <main style={{ position: 'relative' }}>
         <Preloader />
-        <PremiumEffects /> {/* Drops in the global physics and texture */}
+        <PremiumEffects /> 
         <Navbar />
+        
+        {/* THE NEW NARRATIVE FLOW */}
         <Hero />
+        <AITerminal />   {/* <--- Moved up! Immediately engages the user */}
         <About />
         <Skills /> 
         <Projects />
-        <AITerminal />
         <Contact />
+        
       </main>
     </ReactLenis>
   )
